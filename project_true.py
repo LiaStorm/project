3c0054d73e236d49f3dc95cff382157b3bf363c5
LiaStorm <103304686+LiaStorm@users.noreply.github.com>
2023-03-30 12:23:30 +0300

    Update project_true.py

--- project_true.py
+++ project_true.py
@@ -198,13 +198,13 @@ map_mean_salary = html.Div(children=[
         html.Iframe(id='map1', srcDoc=open("MeanSalary.html", 'r').read(), width = "100%", height = "600")
 ], style={'padding': 10, 'flex': 1})
 map_count_prof = html.Div(children=[
-        html.Iframe(id='map2', srcDoc=open("https://raw.githubusercontent.com/LiaStorm/project/main/CountRoles.html", 'r').read(), width = "100%", height = "600")
+        html.Iframe(id='map2', srcDoc=open("CountRoles.html", 'r').read(), width = "100%", height = "600")
 ], style={'padding': 10, 'flex': 1})
 map_oblas_prof = html.Div(children=[
-        html.Iframe(id='map3', srcDoc=open("https://raw.githubusercontent.com/LiaStorm/project/main/OblastiRoles.html", 'r').read(), width = "100%", height = "600")
+        html.Iframe(id='map3', srcDoc=open("OblastiRoles.html", 'r').read(), width = "100%", height = "600")
 ], style={'padding': 10, 'flex': 1})
 map_prof = html.Div(children=[
-        html.Iframe(id='map4', srcDoc=open("https://raw.githubusercontent.com/LiaStorm/project/main/Roles.html", 'r').read(), width = "100%", height = "600")
+        html.Iframe(id='map4', srcDoc=open("Roles.html", 'r').read(), width = "100%", height = "600")
 ], style={'padding': 10, 'flex': 1})
 
 checklist = html.Div(
